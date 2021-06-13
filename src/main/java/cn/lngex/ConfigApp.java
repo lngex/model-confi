0a78db43c950ab9577ca8b6f93632422c835df35
King <1902156923@qq.com>
2021-06-13 17:34:07 +0800

    1

--- src/main/java/cn/lngex/ConfigApp.java
+++ src/main/java/cn/lngex/ConfigApp.java
@@ -1,4 +1,11 @@
 package cn.lngex;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ConfigApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApp.class);
+    }
 }
